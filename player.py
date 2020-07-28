4044c62859cd84f39d5891a8c656536c01a016df
Carl Ortiz <carlortiz7777@gmail.com>
2020-07-27 22:40:54 -0700

    Fixed player class

--- player.py
+++ player.py
@@ -1,70 +1,21 @@
 class Player:
 
     def __init__(self):
-        self.name = self.get_name()
-        self.points_per_game = self.get_points_per_game()
-        self.rebounds_per_game = self.get_rebounds_per_game()
-        self.assists_per_game = self.get_assists_per_game()
-        self.steals_per_game = self.get_steals_per_game()
-        self.blocks_per_game = self.get_blocks_per_game()
+        self.name = input("Player Name: ")
+        self.points_per_game = self.ask("Points Per Game: ")
+        self.rebounds_per_game = self.ask("Rebounds Per Game: ")
+        self.assists_per_game = self.ask("Assists Per Game: ")
+        self.steals_per_game = self.ask("Steals Per Game: ")
+        self.blocks_per_game = self.ask("Blocks Per Game: ")
 
-    def get_name(self):
-        name = input("Player Name: ")
-        return name
-
-    def get_points_per_game(self):
+    def ask(self, question):
         while True:
             try:
-                points_per_game = float(input("Points Per Game: "))
-                if points_per_game >= 0:
-                    return str(points_per_game)
+                value = float(input(question))
+                if value >= 0:
+                    return str(value)
                 else:
                     print("Enter a number above or equal to 0.")
             except ValueError:
                 print("Enter numbers above or equal to 0 only.")
-
-    def get_rebounds_per_game(self):
-        while True:
-            try:
-                rebounds_per_game = float(input("Rebounds Per Game: "))
-                if rebounds_per_game >= 0:
-                    return str(rebounds_per_game)
-                else:
-                    print("Enter a number above or equal to 0.")
-            except ValueError:
-                print("Enter numbers above or equal to 0 only.")
-
-    def get_assists_per_game(self):
-        while True:
-            try:
-                assists_per_game = float(input("Assists Per Game: "))
-                if assists_per_game >= 0:
-                    return str(assists_per_game)
-                else:
-                    print("Enter a number above or equal to 0.")
-            except ValueError:
-                print("Enter numbers above or equal to 0 only.")
-
-    def get_steals_per_game(self):
-        while True:
-            try:
-                steals_per_game = float(input("Steals Per Game: "))
-                if steals_per_game >= 0:
-                    return str(steals_per_game)
-                else:
-                    print("Enter a number above or equal to 0.")
-            except ValueError:
-                print("Enter numbers above or equal to 0 only.")
-
-    def get_blocks_per_game(self):
-        while True:
-            try:
-                blocks_per_game = float(input("Blocks Per Game: "))
-                if blocks_per_game >= 0:
-                    return str(blocks_per_game)
-                else:
-                    print("Enter a number above or equal to 0.")
-            except ValueError:
-                print("Enter numbers above or equal to 0 only.")
-
 
